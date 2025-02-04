f66f2a986ddb2b40b2b2404665263e372cf76f54
Jamaine <jys1g23@soton.ac.uk>
2025-02-04 19:08:13 +0000

    This is a test to see if jira is linked

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello Hello");
-
+        System.out.printf("bing chilling");
+        System.out.printf("スンバラリア星人いるな");
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 9; i++) {
 
