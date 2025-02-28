2ccd7af9f607dcb469b50eb0f94664b876826b90
TunZ04 <jordantun14@gmail.com>
2025-02-28 18:37:16 +0000

    added runway calculations

--- src/main/java/group50/model/Runway.java
+++ src/main/java/group50/model/Runway.java
@@ -1,5 +1,7 @@
 package group50.model;
 
+import static java.lang.Math.*;
+
 public class Runway {
 
   public Runway(String name, int length, int stripLength, int stopway, int clearway, int displacedThreshold, int RESA) {
@@ -8,7 +10,7 @@ public class Runway {
     this.stopway = stopway;
     this.clearway = clearway; //clearway should be greater than stopway
     this.TORA = length;
-    this.ASDA = ASDA + stopway;
+    this.ASDA = length + stopway;
     this.TODA = length + clearway;
 
     this.displacedThreshold = displacedThreshold;
@@ -16,7 +18,7 @@ public class Runway {
 
     this.stripLength = stripLength;
     this.stripEnd = stripLength - length;
-    this.RESA = RESA;
+    this.RESA = max(240, RESA);
 
     this.ALS = new Surface(); //assumes plane's angle of descent is 50:1
     this.TOCS = new Surface();  //assumes plane's angle of ascent is 50:1
@@ -28,6 +30,42 @@ public class Runway {
   private Boolean hasObstacle() {
     return obstacle.getHeight() != 0;
   }
+
+  public void redeclareALL() {
+    TORA = calculateTORA();
+    TODA = calculateTODA();
+    ASDA = calculateASDA();
+    LDA = calculateLDA();
+  }
+
+  private int calculateTORA() {
+    if (obstacle.getDistance() < 0)
+      return round(length - abs(obstacle.getDistance()) - 60 - (RESA + obstacle.getHeight())*TOCS.getAngle() - 0.5f);
+    else
+      return length - obstacle.getDistance() - blastProtection;
+  }
+
+  private int calculateASDA() {
+    if (obstacle.getDistance() < 0)
+      return round(length - abs(obstacle.getDistance()) - 60 - (RESA + obstacle.getHeight())*TOCS.getAngle() - 0.5f);
+    else
+      return length + clearway - obstacle.getDistance() - blastProtection;
+  }
+
+  private int calculateTODA() {
+    if (obstacle.getDistance() < 0)
+      return round(length - abs(obstacle.getDistance()) - 60 - (RESA + obstacle.getHeight())*TOCS.getAngle() - 0.5f);
+    else
+      return length + stopway - obstacle.getDistance() - blastProtection;
+  }
+
+  private int calculateLDA() {
+    if (obstacle.getDistance() < 0)
+      return length - max(abs(obstacle.getDistance()), displacedThreshold);
+    else
+      return round(length - obstacle.getDistance() - 60 - max((RESA + obstacle.getHeight())*TOCS.getAngle(), displacedThreshold) - 0.5f) - displacedThreshold;
+  }
+  
 
   private String name;
 
@@ -53,7 +91,7 @@ public class Runway {
 
   private int stripEnd; //distance between runway and stopway
 
-  private int BlastProtection;  //safety distance behind aircraft
+  private int blastProtection;  //safety distance behind aircraft
 
   private Surface ALS;  //approach landing surface
 
@@ -108,7 +146,7 @@ public class Runway {
   }
 
   public int getBlastProtection() {
-    return BlastProtection;
+    return blastProtection;
   }
 
   public Surface getALS() {
@@ -120,7 +158,7 @@ public class Runway {
   }
 
   public void setBlastProtection(int blastProtection) {
-    this.BlastProtection = blastProtection;
+    this.blastProtection = blastProtection;
   }
 
 
