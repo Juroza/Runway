9f7839576e42b3b4b8c16bc81ed57e35a42bf15e
Juroza <jamaine.scarlett@gmail.com>
2025-03-02 18:42:26 +0000

    remove mistake

--- src/main/java/group50/model/Airport.java
+++ src/main/java/group50/model/Airport.java
@@ -12,26 +12,6 @@ public class Airport {
 
   private List<Runway> runways;
 
-  public int getClearedAndGradedWidth() {
-    return clearedAndGradedWidth;
-  }
-
-  public void setClearedAndGradedWidth(int clearedAndGradedWidth) {
-    this.clearedAndGradedWidth = clearedAndGradedWidth;
-  }
-
-  private int clearedAndGradedWidth;  // Total width of the cleared and graded area (meters)
-
-  public int getClearedAndGradedLengthBeyondRunwayEnds() {
-    return clearedAndGradedLengthBeyondRunwayEnds;
-  }
-
-  public void setClearedAndGradedLengthBeyondRunwayEnds(int clearedAndGradedLengthBeyondRunwayEnds) {
-    this.clearedAndGradedLengthBeyondRunwayEnds = clearedAndGradedLengthBeyondRunwayEnds;
-  }
-
-  private int clearedAndGradedLengthBeyondRunwayEnds;  // Length it extends beyond each end of the runway (meters)
-
   public void addRunway(String name, int TORA, int TODA, int ASDA, int LDA, int clearwayWidth,int clearwayHeight) {
     runways.add(new Runway(name, TORA, TODA, ASDA, LDA,clearwayWidth,clearwayHeight,1));
   }
